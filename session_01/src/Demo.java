810b12f826b1967c23d4c47d61e26760c80814b3
Nikita Shardul <nikitashardul45@gmail.com>
2024-07-23 18:36:37 +0530

    Added some files

---
+++ session_01/src/Demo.java
@@ -0,0 +1,29 @@
+
+public class Demo {
+  private int dd,mm,yy;
+  public void initDate()
+  {
+	  dd=mm=yy=1;
+  }
+  public void setDate(int d, int m, int y)
+  {
+	  dd = d;
+	  mm = m;
+	  yy = y;
+  }
+  public void displayDate()
+  {
+	  System.out.println("Date = ["+dd+"/"+mm+"/"+yy+"]");
+  }
+  
+  public static void main(String[] args) {
+	  //Demo d; //reference var
+	  //d = new Demo();
+	  Demo d = new Demo();
+	  System.out.println("d = "+d.dd);
+	  d.initDate();
+	  d.displayDate();
+	  d.setDate(3, 07, 2024);
+	  d.displayDate();
+  }
+}
